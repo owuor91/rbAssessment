ccf1c330f2bb22bbb377c15fecfcebb3223a624d
owuor91 <owuor91@gmail.com>
2015-05-22 16:36:04 +0300

    Documented solution

--- exception.rb
+++ exception.rb
@@ -1,16 +1,23 @@
 module NoMethod
 	def checkMethod(input)
+
+		#receive user input and check whether the Price object responds to the method that the user has called
 		method = input
 		present = Price.method_defined? method
 
-		method.to_s
+		method.to_s  #convert method name to string so as to interpolate in the prompt below
 
+
+		#if method called on price object does not exist, inform user and prompt them for code to run the non-existent method
 		if present == false
 			puts "Method #{method} is undefined"
 			puts "Please define what #{method} should do. delimit lines with a semicolon"
 			
+			#receive user code and assign it to variable
 			userCode = gets
-					
+
+
+			#dynamically define non-existent method and assign users code to it then patch it to the price object		
 			Price.send(:define_method, "#{method}")do
 			eval(userCode)
 		end
--- ops.rb
+++ ops.rb
@@ -1,23 +1,27 @@
+#Class Price uses checkMethod(input) contained in the NoMethod Module written in the exception.rb file
+
 require_relative 'exception.rb'
 class Price
 
 	include NoMethod
 
+
+#attribute accessor of the Price class
 	def initialize(value)
 		@value = value
 	end
 
+#overriding the ruby interpreter using method_missing to dynamically handle the absence of a mthod called by user on the Price object
 	def method_missing(input)
 		self.checkMethod(input.to_s)
 	end
 
+#calculate VAT on given price. an example of a present method 
 	def tax
 		@value * 0.16
 	end
 
 
-
 end
 
 
-p bei = Price.new(420)
