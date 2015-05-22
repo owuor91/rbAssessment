64340c14cba1e490254e98338a9f344770c13402
owuor91 <owuor91@gmail.com>
2015-05-22 14:20:03 +0300

    noMethod module complete

--- exception.rb
+++ exception.rb
@@ -4,7 +4,19 @@ module NoMethod
 		arr = input.split(".")
 		method = arr[1].to_sym
 		present = Price.method_defined? method
+
+		method.to_s
+
 		if present == false
-			puts "#{method} is undefined"
+			puts "Method #{method} is undefined"
+			puts "Please define what #{method} should do and end with a newline"
+			userCode = gets
+					
+			Price.send(:define_method, "#{method}")do
+			eval(userCode)
+		end
+			
+
+		end
 	end
 end
